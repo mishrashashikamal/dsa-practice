771ad18d63b1ee8fae69765c7cc581ebb7e170ea
Shashi Kamal Mishra <mishrashashikamal7@gmail.com>
2026-06-26 20:32:28 +0530

    solved leet code problem using sliding window , longest substring without repeating characters.

---
+++ sliding window/no_repeat_substring.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+        // Stores frequency of characters inside current window
+        unordered_map<char, int> freq;
+
+        int left = 0;
+        int ans = 0;
+
+        // Expand the window
+        for (int right = 0; right < s.size(); right++) {
+
+            // Include current character
+            freq[s[right]]++;
+
+            // If duplicate exists, shrink window
+            while (freq[s[right]] > 1) {
+
+                // Remove left character from window
+                freq[s[left]]--;
+
+                // Move left pointer
+                left++;
+            }
+
+            // Current window has all unique characters
+            ans = max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};
